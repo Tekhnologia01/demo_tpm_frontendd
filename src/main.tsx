3c8d9015223423476a4a4753a83e6d64ec850df8
Tekhnologia01 <202914674+Tekhnologia01@users.noreply.github.com>
2025-06-03 14:50:55 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Tailwind and custom styles
 import './index.css';
+
+//  Root component
 import App from './App';
+
+// Redux store
 import { store } from './store';
 import { Provider } from 'react-redux';
 
+// Render the app
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
