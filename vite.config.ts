96846782eb71a3ef094891e988a02c080dbd6628
Tekhnologia01 <202914674+Tekhnologia01@users.noreply.github.com>
2025-06-03 14:39:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react()],
   server: {
     host: true,
-    port: 3000
-  }
+    port: 3000,
+  },
 })
